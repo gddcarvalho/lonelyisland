9e342948a9b177425cda3cdca701f70748d459ca
Gabriel Carvalho <nihongabriel@gmail.com>
2016-02-19 13:54:00 -0800

    create first table

--- index.php
+++ index.php
@@ -1,20 +1,38 @@
 <?php 
 $dbconn = pg_connect(getenv('CONN_STRING'));
-    if ($dbconn) {
-        print "Successfully connected to: " . pg_host($dbconn) . "<br/>\n";
-    } else {
-        print pg_last_error($dbconn);
-        exit;
-    }
+
+$query = sprintf("CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` date,
+  `updated_at` date,
+  `inactivation_date` date,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+
+$query .= "CREATE TRIGGER trUserCreationDate ON user
+  FOR INSERT 
+  AS
+  UPDATE user SET user.created_at=getdate()
+  FROM user INNER JOIN Inserted ON user.id= Inserted.id"
+
+$query .= "CREATE TRIGGER trUserUpdateDate ON user
+  FOR UPDATE 
+  AS
+  UPDATE user SET user.updated_at=getdate()
+  FROM user INNER JOIN Inserted ON user.id= Inserted.id"
+
+
+$result = pg_query($dbconn, $query);
+
+if (!$result) {
+  $message  = 'Invalid query: ' . "\n";
+  $message .= 'Whole query: ' . $query;
+  die($message);
+}
 
 pg_close($dbconn);
-
-if(!pg_close($dbconn)) {
-        print "Failed to close connection to " . pg_host($dbconn) . ": " .
-       pg_last_error($dbconn) . "<br/>\n";
-    } else {
-        print "Successfully disconnected from database";
-    }
 
 include_once("./views/home.html"); 
 ?>
