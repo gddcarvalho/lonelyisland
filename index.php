2cb38163e116056084644c31821ff0309fc5baf5
Gabriel Carvalho <nihongabriel@gmail.com>
2016-02-19 11:49:35 -0800

    fix db conn

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php 
 $dbconn = pg_connect(getenv('CONN_STRING'));
-   or die("Could not connect");
-echo "Connected successfully";
+    if ($dbconn) {
+        print "Successfully connected to: " . pg_host($dbconn) . "<br/>\n";
+    } else {
+        print pg_last_error($dbconn);
+        exit;
+    }
+
 pg_close($dbconn);
+
+if(!pg_close($dbconn)) {
+        print "Failed to close connection to " . pg_host($dbconn) . ": " .
+       pg_last_error($dbconn) . "<br/>\n";
+    } else {
+        print "Successfully disconnected from database";
+    }
 
 include_once("./views/home.html"); 
 ?>
