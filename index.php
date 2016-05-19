424e2cd4d392a81ccf671043a11c8176e4cbdf1a
Gabriel Carvalho <nihongabriel@gmail.com>
2016-05-18 20:41:30 -0700

    fix triggers

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ $query1 = "CREATE TABLE members (
   id SERIAL NOT NULL,
   username varchar(100) NOT NULL,
   password varchar(100) NOT NULL,
+  display_name varchar(100),
+  bio varchar(140),
   gc_seals integer,
   position integer,
   created_at date,
@@ -21,7 +23,7 @@ BEGIN
 END;
 $$ language 'plpgsql';";
 
-$query3 = "CREATE TRIGGER update_members_update_at BEFORE UPDATE ON members FOR EACH ROW EXECUTE PROCEDURE update_update_at_column();";
+$query3 = "CREATE TRIGGER update_members_updated_at BEFORE UPDATE ON members FOR EACH ROW EXECUTE PROCEDURE update_update_at_column();";
 
 $query4 =  "CREATE OR REPLACE FUNCTION update_created_at_column()  
 RETURNS TRIGGER AS $$
@@ -31,7 +33,7 @@ BEGIN
 END;
 $$ language 'plpgsql';";
 
-$query5 = "CREATE TRIGGER update_members_update_at BEFORE INSERT ON members FOR EACH ROW EXECUTE PROCEDURE update_created_at_column();";
+$query5 = "CREATE TRIGGER update_members_created_at BEFORE INSERT ON members FOR EACH ROW EXECUTE PROCEDURE update_created_at_column();";
 
 $result1 = pg_query($dbconn, $query1);
 $result2 = pg_query($dbconn, $query2);
