f12a4ca3117c3488a8a8bd7b73550032877f4f73
Gabriel Carvalho <nihongabriel@gmail.com>
2016-05-18 23:44:44 -0700

    fix query print

--- index.php
+++ index.php
@@ -47,7 +47,38 @@ $dbconn = pg_connect(getenv('CONN_STRING'));
 $query = "select * from members;";
 $result = pg_query($dbconn, $query);
 
-printf($result);
+$i = 0;
+
+echo '<html><body><table><tr>';
+
+while ($i < pg_num_fields($result))
+{
+  $fieldName = pg_field_name($result, $i);
+  echo '<td>' . $fieldName . '<td>';
+}
+echo '</tr>';
+
+$i = 0;
+
+while ($row = pg_fetch_row($result)) 
+{
+  echo '<tr>';
+  $count = count($row);
+  $y = 0;
+  while ($y < $count)
+  {
+    $c_row = current($row);
+    echo '<td>' . $c_row . '</td>';
+    next($row);
+    $y = $y + 1;
+  }
+  echo '</tr>';
+  $i = $i + 1;
+}
+
+pg_free_result($result);
+
+echo '</table></body></html>';
 
 pg_close($dbconn);
 
